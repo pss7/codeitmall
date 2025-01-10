256e255025920405e1dd59e40a1972b5057cd170
parksangseok <>
2025-01-10 16:50:38 +0900

    First Commit

---
+++ pages/_app.js
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import { Noto_Sans_KR } from 'next/font/google';
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import { ThemeProvider } from '@/lib/ThemeContext';
+import '@/styles/global.css';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: [],
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Codeitmall</title>
+        <link rel='icon' href='/favicon.ico' />
+        <style>
+          {`
+          html {
+            font-family: ${notoSansKR.style.fontFamily}, sans-serif;
+          }
+         `}
+        </style>
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  )
+}
