8246cc30bc1a25afada4d3e3c127b4f2f55897fb
parksangseok <>
2025-01-10 19:35:54 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import ProductList from '@/components/ProductList';
 import SearchForm from '@/components/SearchForm';
 import axios from '@/lib/axios';
 import styles from '@/styles/Home.module.css';
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
+export async function getStaticProps() {
 
-  async function getProducts() {
-    const res = await axios.get('/products');
-    const nextProducts = res.data.results;
-    setProducts(nextProducts);
+  const res = await axios.get('/products');
+  const products = res.data.results;
+
+  return {
+    props: {
+      products,
+    }
   }
 
-  useEffect(() => {
-    getProducts();
-  }, []);
+}
+
+export default function Home({ products }) {
 
   return (
     <>
